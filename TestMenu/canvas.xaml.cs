e2d77524e24988c6ab60c8eb32b15cd81fe72ad3
AIKADEV <AIKADEV@DESKTOP-PSUC5P9>
2022-10-21 08:52:40 +0700

    Add project files.

---
+++ TestMenu/canvas.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestMenu
+{
+    /// <summary>
+    /// Interaction logic for canvas.xaml
+    /// </summary>
+    public partial class canvas : Window
+    {
+        public canvas()
+        {
+            InitializeComponent();
+        }
+        Point currentPoint = new Point();
+
+      
+        private void Canvas_MouseDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(this);
+        }
+
+        private void Canvas_MouseMove_1(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(this).X;
+                line.Y2 = e.GetPosition(this).Y;
+
+                currentPoint = e.GetPosition(this);
+
+                surface.Children.Add(line);
+            }
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            surface.Children.Clear();
+            
+             
+        }
+    }
+}
